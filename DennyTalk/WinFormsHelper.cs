a620433e06c8e52f5c27e216d4065ec3548a7659
FiftiN <fiftin@outlook.com>
2014-05-12 15:16:50 +0600

    ЧТоб работало под моно

--- DennyTalk/WinFormsHelper.cs
+++ DennyTalk/WinFormsHelper.cs
@@ -39,25 +39,37 @@ namespace DennyTalk
 
         // flash until the window comes to the foreground 
         const int FLASHW_TIMERNOFG = 12;
+		
+		public static bool IsWindows {
+			get {
+				return Environment.OSVersion.Platform == PlatformID.Win32NT
+					|| Environment.OSVersion.Platform == PlatformID.Win32Windows
+					|| Environment.OSVersion.Platform == PlatformID.Win32S;
+			}
+		}
 
-        public static void StartBlinking(Form form)
-        {
-            FLASHWINFO fw = new FLASHWINFO();
-            fw.cbSize = Convert.ToUInt32(Marshal.SizeOf(typeof(FLASHWINFO)));
-            fw.hwnd = form.Handle;
-            fw.dwFlags = FLASHW_TRAY;
-            fw.uCount = UInt32.MaxValue;
-            FlashWindowEx(ref fw);
+        public static void StartBlinking (Form form)
+		{
+			if (IsWindows) {
+				FLASHWINFO fw = new FLASHWINFO ();
+				fw.cbSize = Convert.ToUInt32 (Marshal.SizeOf (typeof(FLASHWINFO)));
+				fw.hwnd = form.Handle;
+				fw.dwFlags = FLASHW_TRAY;
+				fw.uCount = UInt32.MaxValue;
+				FlashWindowEx (ref fw);
+			}
         }
 
-        public static void StopBlinking(Form form)
-        {
-            FLASHWINFO fw = new FLASHWINFO();
-            fw.cbSize = Convert.ToUInt32(Marshal.SizeOf(typeof(FLASHWINFO)));
-            fw.hwnd = form.Handle;
-            fw.dwFlags = FLASHW_STOP;
-            fw.uCount = UInt32.MaxValue;
-            FlashWindowEx(ref fw);
+        public static void StopBlinking (Form form)
+		{
+			if (IsWindows) {
+				FLASHWINFO fw = new FLASHWINFO ();
+				fw.cbSize = Convert.ToUInt32 (Marshal.SizeOf (typeof(FLASHWINFO)));
+				fw.hwnd = form.Handle;
+				fw.dwFlags = FLASHW_STOP;
+				fw.uCount = UInt32.MaxValue;
+				FlashWindowEx (ref fw);
+			}
         }
 
         public static bool IsFocused(Control cont)
